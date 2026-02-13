28b4f5efcea7858b82f5d445f1faadfb942c8e75
aidasofialily-cmd <aidasofialily@gmail.com>
2026-02-13 12:19:43 +0800

    Update joypad_bridge.ml

--- joypad_bridge.ml
+++ joypad_bridge.ml
@@ -6,3 +6,10 @@ let get_joypad_state joypad =
 let () =
   (* Registering the function so C++ can "find" it by string name *)
   Callback.register "get_ocaml_joypad_state" get_joypad_state
+
+  let dump_vram bus filename =
+  let out = open_out_bin filename in
+  for addr = 0x8000 to 0x97FF do
+    output_byte out (Bus.read_byte bus addr)
+  done;
+  close_out out
